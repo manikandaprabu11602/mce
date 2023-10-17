1a5fff1d210d5ea43fdf86a6f35b61948d27a08f
manikandaprabu11602 <manikandaprabu.v@arkinfotec.com>
2023-10-17 12:45:15 +0530

    second change in route

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,6 @@ Route::get('/view', [EditorController::class, 'viewContent'])->name('view-conten
 =======
 //changhs in main branch
 >>>>>>> 736cb514ec4da1233d1ea17185ffdd0a2de22ef0
+
+
+//second change in first branch
