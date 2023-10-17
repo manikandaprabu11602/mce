b9f74c1d5a33f618a2a5f6a7b9f7b24956337d32
manikandaprabu11602 <manikandaprabu.v@arkinfotec.com>
2023-10-17 13:26:49 +0530

    plain

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,3 @@ Route::get('/editor', [EditorController::class, 'index'])->name('editor');
 Route::post('/save-content', [EditorController::class, 'saveContent'])->name('save-content');
 
 Route::get('/view', [EditorController::class, 'viewContent'])->name('view-content');
-
-<<<<<<< HEAD
-//changhs in main branchsssss
-
-//jjjjjjjjjj
-=======
->>>>>>> 6dc61bc138a96435aaa565b67156bcd2f85cbcd6
-
